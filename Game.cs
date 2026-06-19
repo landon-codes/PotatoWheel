867b0a1ec041856013efb455d474a988147a1bcd
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-19 14:07:41 -0500

    Draw the player in the middle of the screen

--- Game.cs
+++ Game.cs
@@ -1,7 +1,9 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
 using Foster.Framework;
-using FosterFlow;
 using FosterFlow.Graphics;
+using PotatoWheel.GameObjects;
 
 var game = new Game();
 game.Run();
@@ -9,6 +11,8 @@ game.Run();
 public class Game : App
 {
    private Batcher _batcher;
+
+   private Potato _player;
 
    public Game() : base(new AppConfig()
    {
@@ -23,7 +27,31 @@ public class Game : App
 
    protected override void Startup()
    {
+      const float spriteScale = 7.0f;
       
+      AtlasGenerator atlasGenerator = new("Assets", GraphicsDevice, [
+         // Player animations
+         Path.Combine("Potato", "PotatoIdle.ase"),
+         Path.Combine("Potato", "Move", "PotatoDown.ase"),
+         Path.Combine("Potato", "Move", "PotatoUp.ase"),
+         Path.Combine("Potato", "Move", "PotatoMoveHorizontal.ase")
+      ]);
+
+      // Create the player sprite
+      Dictionary<string, List<Subtexture>> playerAnimations = new()
+      {
+         {"Idle", [atlasGenerator.GetTexture("PotatoIdle0"),
+                   atlasGenerator.GetTexture("PotatoIdle1")]},
+         {"MoveHorizontal", [atlasGenerator.GetTexture("PotatoMoveHorizontal0"),
+                             atlasGenerator.GetTexture("PotatoMoveHorizontal1"),
+                             atlasGenerator.GetTexture("PotatoMoveHorizontal2")]},
+         {"MoveUp", [atlasGenerator.GetTexture("PotatoUp0"),
+                     atlasGenerator.GetTexture("PotatoUp1")]},
+         {"MoveDown", [atlasGenerator.GetTexture("PotatoDown0"),
+                       atlasGenerator.GetTexture("PotatoDown1")]}
+      };
+      AnimatedSprite playerSprite = new(playerAnimations, "Idle", 0.5f, spriteScale);
+      _player = new Potato(playerSprite, new Vector2(Window.Width / 2.0f, (Window.Height / 2.0f) + 5));
    }
 
    protected override void Shutdown()
@@ -33,11 +61,16 @@ public class Game : App
 
    protected override void Update()
    {
-      
+      _player.Update(Time.Delta);
    }
    
    protected override void Render()
    {
+      Window.Clear(Color.White);
+
+      _player.Draw(_batcher);
       
+      _batcher.Render(Window);
+      _batcher.Clear();
    }
 }
---
+++ GameObjects/Potato.cs
@@ -0,0 +1,25 @@
+﻿using Foster.Framework;
+using FosterFlow.Graphics;
+using FosterFlow.Collisions;
+using System.Numerics;
+
+namespace PotatoWheel.GameObjects;
+
+public class Potato(AnimatedSprite sprite, Vector2 startingPosition)
+{
+    private Vector2 _position = startingPosition;
+
+    public CircleBound Bounds => new CircleBound(_position, sprite);
+
+    public void Update(float deltaTime)
+    {
+        sprite.Update(deltaTime);
+        
+        // Imagine some other fancy logic at the moment.
+    }
+
+    public void Draw(Batcher batcher)
+    {
+        sprite.Draw(batcher, _position);
+    }
+}
