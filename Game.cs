68c2a9a7ef0325d3e739ea797a7ff328d6ac0f27
landon-codes <230772997+landon-codes@users.noreply.github.com>
2026-06-19 13:44:15 -0500

    Create the base template for the game

---
+++ Game.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using Foster.Framework;
+using FosterFlow;
+using FosterFlow.Graphics;
+
+var game = new Game();
+game.Run();
+
+public class Game : App
+{
+   private Batcher _batcher;
+
+   public Game() : base(new AppConfig()
+   {
+      ApplicationName = "PotatoWheel",
+      WindowTitle = "Potato Wheel",
+      Width = 1280,
+      Height = 720
+   })
+   {
+      _batcher = new Batcher(GraphicsDevice);
+   }
+
+   protected override void Startup()
+   {
+      
+   }
+
+   protected override void Shutdown()
+   {
+      
+   }
+
+   protected override void Update()
+   {
+      
+   }
+   
+   protected override void Render()
+   {
+      
+   }
+}
